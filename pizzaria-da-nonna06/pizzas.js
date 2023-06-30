e06c3178712c404720d91bde52d37996838634c3
jayane_santos <100379675+projetosjay@users.noreply.github.com>
2023-06-30 09:47:17 -0300

    Add files via upload

---
+++ pizzaria-da-nonna06/pizzas.js
@@ -0,0 +1,50 @@
+let pizzaJson = [
+  {
+      id: 1,
+      name: 'Donuts de morango',
+      img: 'images/f1.png',
+      price: [15.00,24.54,32.45],
+    },
+    {
+      id: 2,
+      name: 'Donuts com chocolate granulado',
+      img: 'images/f2.png',
+      price: [15.00,24.54,32.45],
+    },
+    {
+      id: 3,
+      name: 'Donuts de sucrilhos de morango',
+      img: 'images/f3.png',
+      price: [15.00,24.54,32.45],
+    },
+    {
+      id: 4,
+      name: 'Donuts de pistache',
+      img: 'images/f4.png',
+      price: [18.00,24.54,32.45],
+    },
+    {
+      id: 5,
+      name: 'Donuts branco',
+      img: 'images/f5.png',
+      price: [15,24.54,32.45],
+    },
+    {
+      id: 6,
+      name: 'Donuts de morango com casquinhas',
+      img: 'images/f6.png',
+      price: [15.00,24.54,32.45],
+    },
+    {
+      id: 7,
+      name: 'Donuts de ovomaltine',
+      img: 'images/f7.png',
+      price: [15.00,24.54,32.45],
+    },
+    {
+      id: 8,
+      name: 'Donuts de chocolate',
+      img: 'images/f8.png',
+      price: [14.00,24.54,32.45],
+    }
+];
